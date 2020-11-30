8f206932131466ec0e13ac5a144432f3ce924432
AOS Automation Release Team <noreply@redhat.com>
2020-11-30 17:08:29 +0000

    Auto-generated commit from 4d10716

--- pipeline-scripts/release.groovy
+++ pipeline-scripts/release.groovy
@@ -765,14 +765,19 @@ def openCincinnatiPRs(releaseName, advisory, candidate_only = false,ghorg = 'ope
                             cat slice00 ul.txt slice01 > ${upgradeChannelFile}
                             git add ${upgradeChannelFile}
                         fi
-                        git commit -m "${pr_title}"
-                        git push -u origin ${branchName}
                         export GITHUB_TOKEN=${access_token}
-                        hub pull-request -b ${ghorg}:master ${labelArgs} -h ${ghorg}:${branchName} ${messageArgs} > ${prefix}.pr
-                        cat ${prefix}.pr >> ${prs_file}    # Aggregate all PRs
+                        git commit -m "${pr_title}"
+                        if [[ "${prefix}" == "candidate" ]]; then
+                            git push origin ${branchName}:master
+                        else
+                            git push -u origin ${branchName}
+                            hub pull-request -b ${ghorg}:master ${labelArgs} -h ${ghorg}:${branchName} ${messageArgs} > ${prefix}.pr
+                            cat ${prefix}.pr >> ${prs_file}    # Aggregate all PRs
+                        fi
                         """
-
-                    prURLs[prefix] = readFile("${prefix}.pr").trim()
+                    if(prefix != 'candidate') {
+                        prURLs[prefix] = readFile("${prefix}.pr").trim()  
+                    }
                 }
             }
 
